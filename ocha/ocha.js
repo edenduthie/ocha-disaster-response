8617c9d99c715fe700d06d02cecd8674e9ca9275
Eden Duthie <eduthie@gmail.com>
2013-10-25 14:31:20 +1100

    Added back to top button

--- ocha/ocha.js
+++ ocha/ocha.js
@@ -2,3 +2,24 @@ $('.pop').popover({
 	placement: 'bottom',
 	html: true
 });
+
+$('.back-to-top').click(function(){
+	$('body').scrollTop(0);
+});
+
+$(document).ready(function(){
+    showHideControl();
+    $(window).scroll(function(){
+        showHideControl();
+    });        
+});
+
+function showHideControl() {
+	var p = $(document).scrollTop();
+	if( p > 200 ) {
+		$('.back-to-top').show();
+	}
+	else {
+		$('.back-to-top').hide();
+	}
+}
